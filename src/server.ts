34373af5d652fbae241ae5331f21469e92e5bd16
Ionut767 <ionutiacob650@gmail.com>
2023-08-28 21:38:45 +0300

    Untracked node_modules from directory

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import yts from "yt-search";
 import bodyParser from "body-parser";
 import { selectedDataInterface } from "./interfaces/Interface";
+import * as validUrl from "valid-url";
 const app: Application = express();
 
 app.use(cors({ credentials: true, origin: true }));
@@ -15,8 +16,15 @@ app.get("/", (req: Request, res: Response) => {
     data: "Hi! To use this API, please check the docs on github! https://github.com/Ionut767/YouTubeVideoDataAPI",
   });
 });
-app.get("/:videoTitle", async (req: Request, res: Response) => {
-  const searchResults = await yts(req.params.videoTitle);
+app.get("/:videoInput", async (req: Request, res: Response) => {
+  let searchResults: any;
+  //const decodedUrl = decodeURIComponent(req.params.videoInput);
+  // if (validUrl.isWebUri(decodedUrl)) {
+  //  searchResults = await yts.search(decodedUrl);
+  //   console.log("Url");
+  // } else {
+  searchResults = await yts(req.params.videoInput);
+  // }
   const attributes: string[] | undefined = req.query.attributes
     ?.toString()
     .split(",");
